e6a4e24c85868709b889583c3aa48d9f504098d8
Vrogdish <vrogdish@outlook.com>
2023-11-06 14:15:03 +0100

    test build

--- src/components/separate/Separate.tsx
+++ src/components/separate/Separate.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 interface Props {
-    className? : string
+  className?: string;
 }
 
-export default function Separate({className}: Props) {
+export default function Separate({ className }: Props) {
   return (
-    <Image 
-    src={"/svg/separate.svg"}
-    alt="separate"
-    width={300}
-    height={20}
-    className={`m-auto h-5 ${className}`}
-  />
-  )
+    <div>
+      {/* <Image
+        src={"/svg/separate.svg"}
+        alt="separate"
+        width={300}
+        height={20}
+        className={`m-auto h-5 ${className}`}
+      /> */}
+    </div>
+  );
 }
